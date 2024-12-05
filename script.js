73c28962255ef913e191864c277b19d23e7716f6
Luke Davies <luke.davies@junitech.com>
2024-12-05 15:12:51 +0000

    fixed broken logic

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let moveList = ["rock", "paper", "scissors", "rock"];
-//let playerMove = moveList[0];
-//let randomNumber = 1;
 
-//let computerMove = moveList[1];
+
 function getResult(playerMove, computerMove){
     if(playerMove == computerMove) {
         return 0 ;
     } 
-    else if (moveList.indexOf(computerMove) == moveList.indexOf(playerMove) + 1 ){
+
+    else if (moveList.indexOf(computerMove) == (moveList.indexOf(playerMove) + 1 ) % (moveList.length - 1)) {
         return -1 ;
     }
+
     else {
         return 1 ;
 
     }
-
     }
-    let result = getResult("scissors", "scissors");
-    console.log(result);
 
+let computerMove = "scissors";
+let playerMove = prompt("Type your move: rock, paper, or scissors"); // would be good to prevent invalid input
+let result = getResult(playerMove, computerMove);
+
+alert(result);
 
